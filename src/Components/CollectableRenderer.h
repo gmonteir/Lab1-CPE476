5ba728fd858798f86404f6f1a246cf1617e7a99e
Nick Ryan <nick.ryan002@gmail.com>
2020-04-20 22:30:48 -0700

    Added Renderers

---
+++ src/Components/CollectableRenderer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef COLLECTABLE_RENDERER_INCLUDED
+#define COLLECTABLE_RENDERER_INCLUDED
+
+#include <glm\glm.hpp>
+#include "Render.h"
+#include "../BaseCode/Shape.h"
+#include "../BaseCode/Texture.h"
+
+using namespace std;
+using namespace glm;
+
+class CollectableRenderer : public Render {
+
+public:
+
+	CollectableRenderer(shared_ptr<Shape> shape, shared_ptr<Program> program, shared_ptr<Texture> texture);
+
+	void update();
+
+};
+
+#endif
--- src/Components/Render.h
+++ src/Components/Render.h
@@ -4,13 +4,25 @@
 #define RENDER_INCLUDED
 
 #include <glm\glm.hpp>
+#include "../BaseCode/Shape.h"
+#include "../BaseCode/Texture.h"
+#include "../BaseCode/Program.h"
+#include "../BaseCode/MatrixStack.h"
+
+using namespace std;
+using namespace glm;
 
 class Render {
 
 public:
 
 	virtual void update();
-	void init();
+
+	shared_ptr<MatrixStack> Projection;
+	shared_ptr<MatrixStack> View;
+	shared_ptr<Shape> shape;
+	shared_ptr<Program> program;
+	shared_ptr<Texture> texture;
 
 };
 
